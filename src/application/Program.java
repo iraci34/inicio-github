b5868f70cf9cf0b0d7d02dda7efa734b82fa419f
iraci34 <netinho.iraci@gmail.com>
2021-10-25 16:32:30 -0300

    Mensagem explicativa

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,17 @@
 package application;
 
 public class Program {
-
+	public static void main(String[] args) {
+     
+	int num1, num2, total;
+	num1 = 10;
+	num2 = 20;
+	total = num1 + num2;
+ 
+	System.out.println("Bom dia");
+	System.out.println("Bom tarde");
+	System.out.println("Bom noite");
+	System.out.println(total);
+	
+	}
 }
